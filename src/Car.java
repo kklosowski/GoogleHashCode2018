96723b71447f8232cd8f0b6a84152e2a01a134f2
Jasiudab <dabrowski.janek@gmail.com>
2018-03-01 18:20:28 +0000

    car can make on time method

--- src/Car.java
+++ src/Car.java
@@ -8,11 +8,22 @@ public class Car {
 
     public Car(int number, Coord position){
         this.number = number;
-        this.position
+        this.position = position;
     }
 
     public boolean canMakeOnTime(Ride ride){
-        Utils
-        if(Utils.)
+        int distance = Utils.distance(position, ride.startIntersection);
+        int arrival = distance + this.time;
+        int tripTime = Utils.distance(ride.startIntersection, ride.finishIntersection);
+        if(arrival < arrival + tripTime){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public void move(){
+
     }
 }
