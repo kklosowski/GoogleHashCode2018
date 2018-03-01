e4da9f92f56c93c94e8fcf7a5c6a16b7b0ab6a8b
Oskar F <ofigura@interia.pl>
2018-03-01 17:14:56 +0000

    Added main and solution classes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
-/**
- * Created by kklosowski on 01/03/2018.
- */
-public class Main {
-    public static void main(String[] args) {
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 
+public class Main {
+
+    //public static final String FILE_NAME = "videos_worth_spreading";
+    public static ArrayList<String> fileNames;
+
+    public static void main(String[] args) {
+        fileNames = new ArrayList<>();
+        fileNames.add("me_at_the_zoo");
+        fileNames.add("videos_worth_spreading");
+        fileNames.add("trending_today");
+        fileNames.add("kittens");
+        try {
+            for (String FILE_NAME: fileNames) {
+                System.out.println(FILE_NAME);
+                List<String> raw = Files.lines(Paths.get("./inputs/" + FILE_NAME + ".in")).collect(Collectors.toList());
+                //Solution solution = new Solution(raw,FILE_NAME);
+                //solution.solve();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
