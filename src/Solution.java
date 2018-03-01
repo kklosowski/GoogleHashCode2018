fef33799a6bc01a9276733e91a0e52c630e62c6a
Oskar F <ofigura@interia.pl>
2018-03-01 17:18:54 +0000

    initial code for solution file reader

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,12 @@
+import java.util.List;
 
 public class Solution {
+    public final String FILE_NAME;
+    private final List<String> raw;
+
+    public Solution(List<String> raw, String filename) {
+        this.FILE_NAME = filename;
+        this.raw = raw;
+
+    }
 }
