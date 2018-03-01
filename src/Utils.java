0c87c7fc07d7190bfaaefcb2636f890e0b42e520
Jasiudab <dabrowski.janek@gmail.com>
2018-03-01 19:55:01 +0000

    Scoring method

--- src/Utils.java
+++ src/Utils.java
@@ -8,4 +8,19 @@ public class Utils {
     public static int distance(Coord c1, Coord c2){
         return Math.abs(c2.y - c1.y) + Math.abs(c2.x - c1.x);
     }
+
+    public static int getArrivalTimeFor(Car car, Ride ride){
+        return distance(car.position, ride.startIntersection) + car.time;
+    }
+
+    public static int score(Car car, Ride ride, int bonus){
+        int result = 0;
+        if(car.canMakeOnTime(ride)) {
+            result = distance(ride.startIntersection, ride.finishIntersection);
+            if(ride.earliestStart == Utils.getArrivalTimeFor(car, ride)){
+                result += bonus;
+            }
+        }
+        return result;
+    }
 }
