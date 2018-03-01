9ab15478f4d188c7eb7da29d47c66f903271b607
Jasiudab <dabrowski.janek@gmail.com>
2018-03-01 18:13:59 +0000

    Car and utils fix

---
+++ src/Car.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Car {
+    public int number;
+    public Coord position;
+    public int time;
+    public List<Ride> history;
+
+    public Car(int number, Coord position){
+        this.number = number;
+        this.position
+    }
+
+    public boolean canMakeOnTime(Ride ride){
+        Utils
+        if(Utils.)
+    }
+}
--- src/Utils.java
+++ src/Utils.java
@@ -4,9 +4,8 @@
  * created on: 01/03/2018.
  */
 public class Utils {
-    public static int distance;
 
-    public void distance(Coord c1, Coord c2){
-        this.distance = Math.abs(c2.y - c1.y) + Math.abs(c2.x - c1.x);
+    public static int distance(Coord c1, Coord c2){
+        return Math.abs(c2.y - c1.y) + Math.abs(c2.x - c1.x);
     }
 }
